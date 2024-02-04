74c4878d4d2147fa0c7a1ca7a4f45ee39690ced4
oleksamor <125139066+oleksamor@users.noreply.github.com>
2024-02-04 10:55:20 +0200

    task1

---
+++ index.js
@@ -0,0 +1,15 @@
+/**
+ *? Використовуючи функцію if...else,
+ *? напишіть код, який запитуватиме:
+ *? "Яка офіційна назва JavaScript?"
+ *? Якщо користувач вводить "ECMAScript",
+ *? то показати через alert: "Вірно!"
+ *? інакше відобразити:"Не знаєте? ECMAScript!"
+ */
+const message = prompt("Яка офіційна назва JavaScript?");
+if (message === "ECMAScript") {
+    alert("Вірно!");
+
+} else {
+    alert ("Не знаєте? ECMAScript!");
+} 
